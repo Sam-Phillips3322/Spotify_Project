7b47fd18e0b6a9acac4c2ab5e0b23c7242b7d342
Sam Phillips <samphillips3322@gmail.com>
2024-12-25 21:44:14 +0000

    Initial commit: Set up Spotify Swipe App

---
+++ public/script.js
@@ -0,0 +1,3 @@
+// This file will eventually hold JavaScript to handle user interactions
+// For now, it's empty, but it can be extended later for swipe functionality
+console.log("Swipefy app is ready!");
